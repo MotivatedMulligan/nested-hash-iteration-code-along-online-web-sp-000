dd98740484fe4f9bbb7a3a03b2f9eb75d3a73ca1
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-21 19:05:34 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,6 +13,16 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
+  contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_ice_cream_flavors: ["chocolate", "vanilla"]},
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]}
+}
 #contacts["Freddie Mercury"] == :favorite_ice_cream_flavors
 def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash| 
