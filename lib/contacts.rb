a62eadcd96bdf9c11d3cdabcec8c03d7907061a7
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-21 18:59:34 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,6 @@ require 'pry'
   # }
 #contacts["Freddie Mercury"] == :favorite_ice_cream_flavors
 def remove_strawberry(contacts)
-contacts.each do |person, favorite_ice_cream_flavor_hash| end
+contacts.each do |person, contact_details_hash| end
 binding.pry
 end
